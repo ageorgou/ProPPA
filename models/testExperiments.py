3435f38217b6e058816992a6dfd810a567ee25f7
Anastasis Georgoulas <hionanthropos11@yahoo.com>
2017-09-13 21:00:53 +0100

    Change into package
    With the intention of creating an installable package for ProPPA, this
    commit adds the scaffolding (__init__.py files) for converting the code to
    proper Python packages. It also updates the experiment examples script
    (models/testExperiments.py) to correctly invoke the main module, along
    with some style changes.

--- models/testExperiments.py
+++ models/testExperiments.py
@@ -5,18 +5,21 @@ Created on Thu Jan 19 12:02:51 2017
 
 @author: Anastasis
 """
+import sys
+
 import numpy as np
 import scipy.stats as spst
 import matplotlib.pyplot as plt
 
+sys.path.append('..')
 import proppa
 
 # ProPPA can be called from the command line (python proppa <model>), or can be
 # used as a library to perform inference or simulation of continuous-time,
 # population-based stochastic systems with uncertain parameters.
 # The following runs each of the three experiments reported in the paper:
-names = ["SIR.proppa","rumour.proppa","predPrey.proppa"] # example models
-n_samples = 100000 # how many samples to take from the posterior
+names = ["SIR.proppa", "rumour.proppa", "predPrey.proppa"]  # example models
+n_samples = 100000  # how many samples to take from the posterior
 results = []
 for model_name in names:
     model = proppa.load_model(model_name)
@@ -30,22 +33,21 @@ for model_name in names:
 # To plot the results as in the paper:
 # all_par_names has the necessary information about how the data was generated
 # format: (name,true_value,lower_limit,upper_limit)
-all_par_names = {'SIR.proppa' : [('r_i',0.4,0,1),('r_r',0.5,0,1)],
-                 'rumour.proppa' : [('k_s',0.5,0,1),('k_r',0.1,0,1)],
-                 'predPrey.proppa' : [('a',0.0001,0,0.001),
-                                      ('b',0.0005,0,0.001),
-                                      ('c',0.0005,0,0.001),
-                                      ('d',0.0001,0,0.001)]
-                }
+all_par_names = {'SIR.proppa': [('r_i', 0.4, 0, 1), ('r_r', 0.5, 0, 1)],
+                 'rumour.proppa': [('k_s', 0.5, 0, 1), ('k_r', 0.1, 0, 1)],
+                 'predPrey.proppa': [('a', 0.0001, 0, 0.001),
+                                     ('b', 0.0005, 0, 0.001),
+                                     ('c', 0.0005, 0, 0.001),
+                                     ('d', 0.0001, 0, 0.001)]}
 for model_name in names:
-    for (i,p) in enumerate(all_par_names[model_name]):
-        plt.hist([s[i] for s in samples],bins=50)
-        plt.xlim([p[2],p[3]])
+    for (i, p) in enumerate(all_par_names[model_name]):
+        plt.hist([s[i] for s in samples], bins=50)
+        plt.xlim([p[2], p[3]])
         par_name = p[0]
-        plt.xlabel("$" + par_name + "$",fontsize=14)
-        plt.ylabel('# of samples',fontsize=14)
-        plt.axvline(p[1],color='r',linewidth=2)
-        filename = ("hist_" + model_name.rsplit(sep=".",maxsplit=1)[0] + "_" +
+        plt.xlabel("$" + par_name + "$", fontsize=14)
+        plt.ylabel('# of samples', fontsize=14)
+        plt.axvline(p[1], color='r', linewidth=2)
+        filename = ("hist_" + model_name.rsplit(sep=".", maxsplit=1)[0] + "_" +
                     par_name + ".pdf")
         plt.savefig(filename)
         plt.show()
@@ -55,25 +57,25 @@ for model_name in names:
 model_name = 'predPrey.proppa'
 samples = results[2]
 pars = all_par_names[model_name]
-colors = ['teal','blue','green','black']
-line_colors=['red','red','red','red']
-xmin, xmax = pars[0][2:4] # all parameters have the same limits
-x_range = np.linspace(xmin,xmax,101)
-y = spst.gamma.pdf(x_range,a=4,scale=1/10000) # prior pdf
+colors = ['teal', 'blue', 'green', 'black']
+line_colors = ['red', 'red', 'red', 'red']
+xmin, xmax = pars[0][2:4]  # all parameters have the same limits
+x_range = np.linspace(xmin, xmax, 101)
+y = spst.gamma.pdf(x_range, a=4, scale=1/10000)  # prior pdf
 form = plt.ScalarFormatter(useMathText=True)
-form.set_powerlimits((0,0))
+form.set_powerlimits((0, 0))
 
-for (i,p) in enumerate(pars):
-    plt.hist([s[i] for s in samples],bins=50,color=colors[i])
-    plt.axvline(p[1],color=line_colors[i],linewidth=2)
-    plt.xlim([xmin,xmax])
+for (i, p) in enumerate(pars):
+    plt.hist([s[i] for s in samples], bins=50, color=colors[i])
+    plt.axvline(p[1], color=line_colors[i], linewidth=2)
+    plt.xlim([xmin, xmax])
     scaling = plt.ylim()[1] / np.max(y)
-    plt.plot(x_range,y * scaling, 'k--')
+    plt.plot(x_range, y * scaling, 'k--')
     plt.gca().xaxis.set_major_formatter(form)
     par_name = p[0]
-    plt.xlabel("$" + par_name + "$",fontsize=14)
-    plt.ylabel('# of samples',fontsize=14)
-    filename = "".join(["hist_",model_name.rsplit(sep=".",maxsplit=1)[0],"_",
-                       par_name,".pdf"])
+    plt.xlabel("$" + par_name + "$", fontsize=14)
+    plt.ylabel('# of samples', fontsize=14)
+    filename = "".join(["hist_", model_name.rsplit(sep=".", maxsplit=1)[0],
+                        "_", par_name, ".pdf"])
     plt.savefig(filename)
     plt.show()
